29088fa3725db94a89dc22ab19f20ed1c5885a35
Zachattack221 <zsvd09@gmail.com>
2022-09-17 18:33:48 -0400

    testing commits to heroku server

--- public/server.js
+++ public/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3000;
 
 require('./routes/apiRoutes.js')(app);
 require('./routes/htmlRoutes.js')(app);
