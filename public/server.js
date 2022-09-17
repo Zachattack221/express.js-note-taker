a8594dd5ce24182547894219b911a87e371eefa6
Zachattack221 <zsvd09@gmail.com>
2022-09-17 14:06:39 -0400

    updated .gitignore file, working on setting correct get requests in routes folder

--- public/server.js
+++ public/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/indexRoutes.js');
+const api = require('./routes/apiRoutes.js');
 
 const app = express();
 const PORT = process.env.port || 3001;
@@ -10,3 +10,10 @@ app.use(express.static('public'));
 // set middleware to parse JSON and urlencoded data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+  });
