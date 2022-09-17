88665092d71870c7e42ce31745745c907a6a917e
Zachattack221 <zsvd09@gmail.com>
2022-09-17 16:15:23 -0400

    updated logic for htmlRoutes.js and apiRoutes.js to handle GET and POST requests

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,2 +1,43 @@
+const path = require('path');
+// const { application } = require('express');
 const fs = require('fs');
 
+// pulled in uuid dependency to assign notes a unique id
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = function (app) {
+
+    app.get('/api/notes', (req, res) => {
+        console.log('GET notes request sent');
+
+        let note = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+        res.json(note);
+    });
+
+    app.post('/api/notes', (req, res) => {
+
+        const newNote = request.body;
+
+        console.log('POST request sent');
+
+        // pulls in unique id
+        newNote.id = uuidv4();
+
+        let note = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+        // adds new note into db.json 
+        note.push(newNote);
+
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
+        console.log('Note added successfully');
+
+        res.json(note);
+    })
+
+    application.delete('/api/notes/:id', (req, res) => {
+
+
+    });
+};
--- public/server.js
+++ public/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/apiRoutes.js');
 
 const app = express();
 const PORT = process.env.port || 3001;
+
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 app.use(express.static('public'));
 
 // set middleware to parse JSON and urlencoded data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 
 app.listen(PORT, () => {
