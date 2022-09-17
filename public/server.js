85bf9bdb9e0c0ab8e60db0a38b630aa726e08ccb
Zachattack221 <zsvd09@gmail.com>
2022-09-17 19:30:58 -0400

    heroku linked to repository, troubleshooting routing to open application

--- public/server.js
+++ public/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.port || 3000;
 
-require('./routes/apiRoutes.js')(app);
-require('./routes/htmlRoutes.js')(app);
-
-app.use(express.static('public'));
+const apiRoutes = require('./routes/apiRoutes')(app);
+const htmlRoutes = require('./routes/htmlRoutes')(app);
 
 // set middleware to parse JSON and urlencoded data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+    console.log(`App listening on port: ${PORT}`)
   });
